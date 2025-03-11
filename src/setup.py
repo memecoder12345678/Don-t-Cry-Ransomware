64d1fa0e7b40ef107a04068f210ac1a83db0b2bf
memecoder <memecoder17@gmail.com>
2025-03-11 19:33:16 +0700

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("EDFile.pyx", compiler_directives={"language_level": "3"})
+    ext_modules=cythonize("file_crypto.pyx", compiler_directives={"language_level": "3"})
 )
